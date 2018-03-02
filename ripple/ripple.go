9ec131ae2934d7ea36c7a26377e4049f3774c532
Matt McMurray <mathieumcmurray@gmail.com>
2018-03-02 12:31:33 -0600

    Method to sign a xaction

--- ripple/ripple.go
+++ ripple/ripple.go
@@ -7,9 +7,22 @@ import (
 	"net/http"
 )
 
+type transaction struct {
+	Account         string `json:"Account"`
+	Amount          int    `json:"Amount"`
+	Destination     string `json:"Destination"`
+	TransactionType string `json:"TransactionType"`
+}
+
+type param struct {
+	Offline bool        `json:"offline"`
+	Secret  string      `json:"secret"`
+	TxJSON  transaction `json:"tx_json"`
+}
+
 type request struct {
-	Method string        `json:"method"`
-	Params []interface{} `json:"params,omitempty"`
+	Method string  `json:"method"`
+	Params []param `json:"params"`
 }
 
 // Response a response from the rippled server
@@ -41,7 +54,10 @@ var rippledURL = "http://0.0.0.0:5005" // 5005 is the admin port
 
 // Ping Pings the ripple server
 func Ping() (*Response, error) {
-	r := request{"ping", nil}
+	r := request{
+		Method: "ping",
+		Params: nil,
+	}
 
 	p, err := json.Marshal(r)
 	if err != nil {
@@ -57,6 +73,42 @@ func Ping() (*Response, error) {
 	}
 
 	return &pr, nil
+}
+
+// SignXaction sign a payment transaction using a rippled server
+func SignXaction(secret string, account string, destination string, amount int) (*Response, error) {
+	r := request{Method: "sign"}
+	t := transaction{
+		Account:         account,
+		Amount:          amount,
+		Destination:     destination,
+		TransactionType: "Payment",
+	}
+	p := param{
+		Offline: false,
+		Secret:  secret,
+		TxJSON:  t,
+	}
+	r.Params = make([]param, 0)
+	r.Params = append(r.Params, p)
+
+	payload, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := makeHTTPRequest(rippledURL, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	var res *Response
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
 
 func makeHTTPRequest(url string, payload []byte) ([]byte, error) {
